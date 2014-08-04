22a3131d546c94a4e81e1d111b90dc1bfc87de65
maianess <maia.mcc@gmail.com>
2014-08-04 15:06:11 -0400

    added function skeleton

--- markovgen.py
+++ markovgen.py
@@ -3,3 +3,10 @@ class Markov(object):
 		self.corpus = open(corpus_file)
 		self.text = self.corpus.read()
 		self.words = self.text.split()
+		self.dictionary = self.make_dictionary()
+
+	def triples():
+
+	def make_dictionary(self):
+
+	def generate(length=100):
