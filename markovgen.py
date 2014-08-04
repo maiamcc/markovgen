fcedfa87e68d25d1d159a6c2cb0088e1a60045f7
maianess <maia.mcc@gmail.com>
2014-08-04 16:18:37 -0400

    triples working with 'print'

--- markovgen.py
+++ markovgen.py
@@ -1,24 +1,21 @@
 class Markov(object):
-	def __init__(self, corpus_file):
-		self.corpus = open(corpus_file)
-		self.text = self.corpus.read()
-		self.words = self.text.split()
-		self.dictionary = self.make_dictionary()
+    def __init__(self, corpus_file):
+        self.corpus = open(corpus_file)
+        self.text = self.corpus.read()
+        self.words = self.text.split()
+        self.dictionary = self.make_dictionary()
 
-	def triples():
-		"""Prepare to make word triplets to feed the dictionary function."""
-		#for x in range(0, 40):
-            #print words[x], words[x+1], words[x+2]
+    def triples(self):
+        """Prepare to make word triplets to feed the dictionary function."""
+        for x in range(0, 40):
+            print self.words[x], self.words[x+1], self.words[x+2]
 
         #for x in range(0, len(words), -3):
-			#yield words[x], words[x+1], words[x+2]
+            #yield words[x], words[x+1], words[x+2]
+    def make_dictionary(self):
+        """Take every two words as key and third one as value."""
 
-
-	def make_dictionary(self):
-		"""Take every two words as key and third one as value."""
-
-	def generate(length=100):
-		"""Make random text out given length for number of words."""
+    def generate(length=100):
+        """Make random text out given length for number of words."""
 
 pp = Markov("test_data/pride_and_prejudice.txt")
-
