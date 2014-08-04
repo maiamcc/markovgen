c4b45ae9cf97a6abce44113107158a092575fa05
maianess <maia.mcc@gmail.com>
2014-08-04 16:15:22 -0400

    set up to run from ipython

--- markovgen.py
+++ markovgen.py
@@ -7,9 +7,12 @@ class Markov(object):
 
 	def triples():
 		"""Prepare to make word triplets to feed the dictionary function."""
+		#for x in range(0, 40):
+            #print words[x], words[x+1], words[x+2]
 
-		for x in range(0, len(words), -3):
-			yield(words[x], words[x+1], words[x+2])
+        #for x in range(0, len(words), -3):
+			#yield words[x], words[x+1], words[x+2]
+
 
 	def make_dictionary(self):
 		"""Take every two words as key and third one as value."""
@@ -17,4 +20,5 @@ class Markov(object):
 	def generate(length=100):
 		"""Make random text out given length for number of words."""
 
+pp = Markov("test_data/pride_and_prejudice.txt")
 
